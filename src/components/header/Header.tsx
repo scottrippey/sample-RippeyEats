067d9e2768ee3e54284e716051d014f095e94129
Scott Rippey <scottrippey@invisionapp.com>
2020-09-01 07:56:21 -0600

    [header] Clickable menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,12 +9,26 @@ const PLACEHOLDER_TEXT = "e.g. delicious sandwiches";
 export const Header: React.FC = () => {
   const [selectedIndex, setSelectedIndex] = React.useState(1);
   const [searchOpen, setSearchOpen] = React.useState(false);
+  const [navOpen, setNavOpen] = React.useState(true /*todo false*/);
 
   return <header className="text-white text-16">
 
     <h1 className="bg-blue-light px-20 lg:px-40 justify-between leading-52 flex items-center">
-      <span className="lg:hidden">
-        <MenuSvg className="w-35" />
+      <span
+        className={classNames(
+          "lg:hidden -ml-20 p-20 inline-block",
+          "cursor-pointer select-none",
+          { "bg-blue": navOpen },
+        )}
+        onClick={(ev) => {
+          ev.preventDefault();
+          setNavOpen(open => !open);
+        }}
+      >
+        { !navOpen
+          ? <MenuSvg className="w-20" />
+          : <CloseSvg className="w-20" />
+        }
       </span>
       <span className="mr-15 text-center">
         <LogoSvg className="h-35"  />
@@ -48,12 +62,15 @@ export const Header: React.FC = () => {
     </h1>
 
 
-    <h2 className="bg-blue px-20 lg:px-40 leading-52 flex">
+    <h2 className={classNames(
+      "bg-blue lg:px-40 leading-52 flex flex-col lg:flex-row lg:flex",
+      { "hidden": !navOpen }
+    )}>
       { NAV_ITEMS.map(({ id, label }, index) =>
         <a
           key={id}
           className={classNames(
-            "inline-block px-20 first:-ml-20",
+            "inline-block px-20 lg:first:-ml-20 border-white border-t first:border-0 lg:border-none",
             "underline cursor-pointer select-none whitespace-no-wrap",
             { "bg-blue-dark text-yellow": index === selectedIndex }
           )}
